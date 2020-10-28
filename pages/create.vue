fc472ba7fb5967f95dcbed95386d78f3c9061456
robertoshimizu <rshimizubr@gmail.com>
2020-10-28 18:15:32 -0300

    style: :lipstick: revert css style from vue mastery course

--- pages/create.vue
+++ pages/create.vue
@@ -1,18 +1,6 @@
 <template>
-  <div
-    class="container mx-auto bg-blue-100 box-border h-100 w-full p-4 border-gray-400 flex space-y-10"
-  >
-    <h1 class="font-sans text-2xl text-gray-800 text-start justify-left">
-      Create An Event
-    </h1>
-    <div>
-      <!-- Using utilities -->
-      <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Button
-      </button>
-    </div>
+  <div>
+    <h1>Create An Event</h1>
   </div>
 </template>
 <script>
