72166c3799b48735732f1d1a3f8ecc69a8602102
robertoshimizu <rshimizubr@gmail.com>
2020-11-02 01:26:47 -0300

    feat: :boom: add article and render _slug.vue

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,48 @@
+<template>
+  <article>
+    <!-- <pre> {{ article }} </pre> -->
+    <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <img
+      :src="require(`~/assets/images/${article.image}`)"
+      :alt="article.alt"
+    />
+    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+    <nuxt-content :document="article" />
+  </article>
+</template>
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
+
+    return { article }
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
+    },
+  },
+}
+</script>
+<style>
+.nuxt-content h2 {
+  font-weight: bold;
+  font-size: 28px;
+}
+.nuxt-content h3 {
+  font-weight: bold;
+  font-size: 22px;
+}
+.nuxt-content p {
+  margin-bottom: 20px;
+}
+.icon.icon-link {
+  background-image: url('~assets/svg/icon-hashtag.svg');
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-size: 20px 20px;
+}
+</style>
