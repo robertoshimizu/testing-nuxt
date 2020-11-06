901169aeca16eb93021e4151e2d63a9b137981f0
robertoshimizu <rshimizubr@gmail.com>
2020-11-06 20:06:08 -0300

    perf: :alien: create Events Vuex module to call eventservice

--- pages/index.vue
+++ pages/index.vue
@@ -11,17 +11,14 @@
 </template>
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import { mapState } from 'vuex'
 export default {
   components: {
     EventCard,
   },
-  async asyncData({ error }) {
+  async asyncData({ store, error }) {
     try {
-      const { data } = await EventService.getEvents()
-      return {
-        events: data,
-      }
+      await store.dispatch('events/fetchEvents')
     } catch (e) {
       error({
         statusCode: 503,
@@ -29,6 +26,9 @@ export default {
       })
     }
   },
+  computed: mapState({
+    events: (state) => state.events.events,
+  }),
   head() {
     return {
       title: 'Event Listing',
